64ae1313ebed98f60b8ec5fe4230c0f10ce4785e
Jayachand Bokka <jayachand2001@gmail.com>
2020-10-22 20:25:57 +0530

    added forgot password and email verification

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 from .views import register, login_view
 from django.contrib.auth import views as auth_views
+from accounts import views
 
 urlpatterns = [
-    # URLs are of the form users/<name>
+    # URLs are of the form user/<name>
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('activate/<uidb64>/<token>/',views.activate, name='activate'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,17 +2,41 @@ from django.shortcuts import render, redirect
 from .admin import DumboUserCreationForm
 from .forms import DumboUserLoginForm
 from django.contrib.auth import login, logout
-
+from django.core.mail import EmailMessage
+from .tokens import account_activation_token
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import DumboUser
+from django.utils.encoding import force_bytes, force_text
 
 # Create your views here.
 def register(request):  # This view is used to register new users into the application
     if request.method == 'POST':
         form = DumboUserCreationForm(request.POST)  # The form used for saving/creating the new users
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')  # Get the username, can be used later to display a message
-            print(f'Registration successful for {username}')
-            return redirect('login')  # Redirect to the login page after a successful registration
+            # We save the object temporarily and not commit
+            user = form.save(commit=False)
+            # We activate a user account after email confirmation
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your HomeChef account.'
+            message = render_to_string('accounts/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+            mail_subject, message, to=[to_email]
+            )
+            # Email for account verification sent
+            email.send()
+            return render(request, 'accounts/email_confirm_request.html')
     else:
         form = DumboUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
@@ -32,3 +56,19 @@ def login_view(request):
     else:
         form = DumboUserLoginForm()
     return render(request, 'accounts/login.html', {'form': form})
+# View for activation link
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = DumboUser.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, DumboUser.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        # Verified the user, thus made active
+        user.is_active = True
+        user.save()
+		# Logged in
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+        return render(request,'accounts/email_confirm.html')
+    else:
+        return HttpResponse('Activation link is invalid!')
