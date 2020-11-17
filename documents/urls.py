5266e55bd95a87ace253cb172fb8ff93d69ef08e
Lokesh <39303879+Lokesh-pavan-kumar@users.noreply.github.com>
2020-11-17 22:10:54 +0530

    Update urls.py

--- documents/urls.py
+++ documents/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('search', views.SearchResultsView.as_view(), name="search_results"),
     path('toggle-trash/<int:pk>', views.toggle_trash, name='toggle-trash'),
     path('delete-document/<int:pk>', views.delete_document, name='delete-document'),
-    path('trash', views.trashed_documents, name='trash')
+    path('trash', views.trashed_documents, name='trash'),
+    path('download', views.DownloadFile, name="download_file"),
 ]
