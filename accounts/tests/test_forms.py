395cb066272867992dabed7057207a10d13caa04
rajesh-kumar-p <58870827+rajesh-kumar-p@users.noreply.github.com>
2020-11-22 10:38:52 +0530

    added some more tests

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,33 +1,26 @@
 from django.test import SimpleTestCase
 from accounts.forms import UserUpdateForm, ProfileUpdateForm, DumboUserLoginForm
+from documents.forms import UploadDocumentForm, DocumentUpdateForm, DownloadDocumentForm
 
 class TestForm(SimpleTestCase):
-	"""def test_DumboUserLoginForm_valid_data(self):
-					form = DumboUserLoginForm(data = {
-						'identifier': 'jayachand',
-						'password': 'chand@2001'
-					})
-			
-					#self.assertTrue(form.is_valid())
-			
-				def test_DumboUserLoginForm_no_data(self):
-					form = DumboUserLoginForm(data = {})
-					self.assertFalse(form.is_valid())
-					self.assertEquals(len(form.errors), 2)"""
 
 
 	def test_UserUpdateForm_valid_data(self):
 		form = UserUpdateForm(data = {
-			'fullname': 'jayachand',
+			'fullname': 'rajesh',
 			'phone_number': 9999999999
 		})
 
 		self.assertTrue(form.is_valid())
 
+
 	def test_UserUpdateForm_no_data(self):
 		form = UserUpdateForm(data = {})
 		self.assertFalse(form.is_valid())
 		self.assertEquals(len(form.errors), 2)
+
+
+
 
 	def test_ProfileUpdateForm_valid_data(self):
 		form = ProfileUpdateForm(data = {
@@ -41,6 +34,48 @@ class TestForm(SimpleTestCase):
 		form = ProfileUpdateForm(data = {})
 		self.assertFalse(form.is_valid())
 		self.assertEquals(len(form.errors), 1)
+
+class test_document_forms(SimpleTestCase):
+
+	def test_UploadDocumentForm_valid_data(self):
+		form = UploadDocumentForm(data = {
+			'name': 'rajesh',
+			'is_important': True,
+			'is_public': True,
+			'path': 'gs://{bucket-name}/{file-path}'
+
+		})
+		#self.assertTrue(form.is_valid())
+
+	def test_UploadDocumentForm_no_data(self):
+		form = UploadDocumentForm(data = {})
+		self.assertFalse(form.is_valid())
+		self.assertEquals(len(form.errors), 2)
+
+	def test_DocumentUpdateForm_valid_data(self):
+		form = DocumentUpdateForm(data = {
+			'is_important': True,
+			'is_public': True,
+			'expiry_date': 11/11/2020
+			
+		})
+		#self.assertTrue(form.is_valid())
+
+	def test_DocumentUpdateForm_no_data(self):
+		form = DocumentUpdateForm(data = {})
+		self.assertFalse(form.is_valid())
+		self.assertEquals(len(form.errors), 1)
+
+	def test_DownloadDocumentForm_valid_data(self):
+		form = DownloadDocumentForm(data = {
+			'document_id': 'rajesh123',
+		})
+		self.assertFalse(form.is_valid())
+
+	def test_DownloadDocumentForm_no_data(self):
+		form = DownloadDocumentForm(data = {})
+		self.assertFalse(form.is_valid())
+		self.assertEquals(len(form.errors), 2)
 
 
 
